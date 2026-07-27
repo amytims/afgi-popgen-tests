79d9a36d1d7bea81059f412c77327a75d651155e
Amy Tims <atims@setonix-03.head.cm.setonix.pawsey.org.au>
2026-07-27 13:21:50 +0800

    swapping pattern match for actual filename

--- 05_download_and_index_ref.sh
+++ 05_download_and_index_ref.sh
@@ -17,13 +17,12 @@ RCLONE_FILE_PATH='pawsey1228:afgi.atims/reference_genomes/e_rankini/e_rankini_OG
 
 #REF_URL=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/041/903/045/GCA_041903045.1_ASM4190304v1/GCA_041903045.1_ASM4190304v1_genomic.fna.gz
 REF_DIR=/home/atims/pop_gen/pop_gen/reference_genome/$SPECIES
-#REFERENCE=GCA_041903045.1_ASM4190304v1_genomic.fna.gz
 
 mkdir $REF_DIR -p
 
 rclone copy ${RCLONE_FILE_PATH} ${REF_DIR}
 #wget $REF_URL -O ${REF_DIR}/${REFERENCE}
 
-gunzip ${REF_DIR}/*.gz
+gunzip ${REF_DIR}/$(basename ${RCLONE_FILE_PATH})
 
 bwa index ${REF_DIR}/$(basename --suffix=.gz ${RCLONE_FILE_PATH})
