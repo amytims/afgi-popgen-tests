4c94babbf4ee18f86b46c0d8c064347ab08c4de3
Amy Tims <atims@setonix-01.head.cm.setonix.pawsey.org.au>
2026-07-27 14:32:31 +0800

    edit to run as array job

--- 04_merge_lanes.sh
+++ 04_merge_lanes.sh
@@ -2,31 +2,35 @@
 #SBATCH --account=pawsey1132
 #SBATCH --partition=work
 #SBATCH --job-name=merge_lanes_e_rankini
-#SBATCH --out=pop_gen/slurm/merge_lanes_e_rankini_slurm-%j.out
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks=1
 #SBATCH --mem=7G
-#SBATCH --time=12:00:00
+#SBATCH --time=4:00:00
+#SBATCH --array=1-60
+#SBATCH --out=pop_gen/slurm/merge_lanes_slurm-%A_%a.out
 
 
 SPECIES=e_rankini
+
 READS_DIR=/home/atims/pop_gen/pop_gen/trimmomatic_output/${SPECIES}
 MERGED_DIR=/home/atims/pop_gen/pop_gen/merged_reads/${SPECIES}
 
 mkdir -p $MERGED_DIR
 
+file=$(ls pop_gen/trimmomatic_output/e_rankini/*.trim_pe.fastq.gz | xargs -n 1 basename --suffix=.trim_pe.fastq.gz | sed -E 's/_L..._R.$//g' | uniq | awk -v id="${SLURM_ARRAY_TASK_ID}" 'NR==id {print $1}')
 
-for file in $(ls ${READS_DIR}/*.trim_pe.fastq.gz | xargs -n 1 basename --suffix=.trim_pe.fastq.gz | sed -E 's/_L..._R.$//g' | uniq)
-do
+
+#for file in $(ls ${READS_DIR}/*.trim_pe.fastq.gz | xargs -n 1 basename --suffix=.trim_pe.fastq.gz | sed -E 's/_L..._R.$//g' | uniq)
+#do
 	echo $file
 
-	cat $READS_DIR/${file}_L???_R1.trim_pe.fastq.gz > $MERGED_DIR/${file}_merged_R1.trim_pe.fastq.gz &
-	cat $READS_DIR/${file}_L???_R2.trim_pe.fastq.gz > $MERGED_DIR/${file}_merged_R2.trim_pe.fastq.gz &
+	cat $READS_DIR/${file}_L???_R1.trim_pe.fastq.gz > $MERGED_DIR/${file}_merged_R1.trim_pe.fastq.gz
+	cat $READS_DIR/${file}_L???_R2.trim_pe.fastq.gz > $MERGED_DIR/${file}_merged_R2.trim_pe.fastq.gz
 
-	cat $READS_DIR/${file}_L???_R1.trim_se.fastq.gz > $MERGED_DIR/${file}_merged_R1.trim_se.fastq.gz &
-	cat $READS_DIR/${file}_L???_R2.trim_se.fastq.gz > $MERGED_DIR/${file}_merged_R2.trim_se.fastq.gz &
+	cat $READS_DIR/${file}_L???_R1.trim_se.fastq.gz > $MERGED_DIR/${file}_merged_R1.trim_se.fastq.gz
+	cat $READS_DIR/${file}_L???_R2.trim_se.fastq.gz > $MERGED_DIR/${file}_merged_R2.trim_se.fastq.gz
 
-wait
+#echo "finished running file: $file"
+#done
 
-echo "finished running file: $file"
-done
+#ls $MERGED_DIR/*.merged_R1.trim_pe.fastq.gz > $MERGED_DIR/${SPECIES}_merged_filenames.txt
