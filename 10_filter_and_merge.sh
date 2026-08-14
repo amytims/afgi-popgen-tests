551241a58c047a0a5ce5ab9d57baf42e5617d15f
Amy Tims <atims@setonix-01.head.cm.setonix.pawsey.org.au>
2026-08-14 13:33:25 +0800

    edits to filtering process

--- 10_filter_and_merge.sh
+++ 10_filter_and_merge.sh
@@ -6,19 +6,29 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --mem=1800MB
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 
 
 module load bcftools/1.15--haf5b3da_0
 
 SPECIES=e_rankini
-VARIANTS=/home/atims/pop_gen/pop_gen/variant_calling/$SPECIES
-FILTER=/home/atims/pop_gen/pop_gen/filtered_variants/$SPECIES
+VARIANTS=/home/atims/pop_gen/pop_gen/variant_calling/${SPECIES}_2
+FILT=/home/atims/pop_gen/pop_gen/filtered_variants/${SPECIES}_2
 
-mkdir -p $FILTER
+mkdir -p $FILT
 
-# merge all files
-bcftools concat $VARIANTS/*.vcf -O z -o $FILT/unfiltered_data.vcf.gz
+# filter files to contain only snp sites; makes everything else quicker because the full files are huge
+#for i in {01..24}; do
+#	bcftools view -v snps,indels -o pop_gen/variant_calling/e_rankini_2/markdup_${i}_snps_only.vcf \
+#	pop_gen/variant_calling/e_rankini_2/markdup_${i}.vcf
+#done
+
+
+
+# merge all files and sort
+#bcftools concat $VARIANTS/*_snps_only.vcf | bcftools sort --temp-dir $FILT -O z -o $FILT/unfiltered_data_snps_only.vcf.gz
+
+#bcftools stats $FILT/unfiltered_data_snps_only.vcf.gz > $FILT/unfiltered_data_snps_only.stats
 
 # filter
 # SNPS
@@ -26,7 +36,73 @@ bcftools concat $VARIANTS/*.vcf -O z -o $FILT/unfiltered_data.vcf.gz
 # biallelic sites only
 # min allele frequency >5%
 
+#remove anything within 5bp of an indel
+#keep only biallelic SNPs with no missing samples
+##      -v snps -m2 -M2 -g ^miss \
+bcftools filter -g 5:indel,other $FILT/unfiltered_data_snps_only.vcf.gz |
 bcftools view \
-	-v snps -m2 -M2 --min-ac 1:minor -i 'QUAL>=30' -g ^miss \
-	-O z -o $FILT/filtered_data_1.vcf.gz \
-	$FILT/unfiltered.data.vcf.gz
+       -v snps -m2 -M2 -g ^miss \
+	-O z -o $FILT/filtered_data_1.vcf.gz
+
+bcftools stats $FILT/filtered_data_1.vcf.gz > $FILT/filtered_data_1.stats
+
+# remove sites with maf < 0.05
+bcftools view \
+        -q 0.05:minor \
+        -O z -o $FILT/filtered_data_2.vcf.gz \
+        $FILT/filtered_data_1.vcf.gz
+
+bcftools stats $FILT/filtered_data_2.vcf.gz > $FILT/filtered_data_2.stats
+
+
+# filter any sites with >2.5x the average read depth
+bcftools filter -e 'INFO/DP>2.5*AVG(INFO/DP)' \
+        -O z -o $FILT/filtered_data_3.vcf.gz \
+        $FILT/filtered_data_2.vcf.gz
+
+bcftools stats $FILT/filtered_data_3.vcf.gz > $FILT/filtered_data_3.stats
+
+# filter any low-coverage sites - mean read depth < 10
+bcftools filter -e 'INFO/DP<600' \
+        -O z -o $FILT/filtered_data_4.vcf.gz \
+        $FILT/filtered_data_3.vcf.gz
+
+bcftools stats $FILT/filtered_data_4.vcf.gz > $FILT/filtered_data_4.stats
+
+
+# map quality
+bcftools filter -e 'MQ<30' \
+        -O z -o $FILT/filtered_data_5.vcf.gz \
+        $FILT/filtered_data_4.vcf.gz
+
+bcftools stats $FILT/filtered_data_5.vcf.gz > $FILT/filtered_data_5.stats
+
+
+# genotype quality
+bcftools filter -e 'QUAL<30' \
+        -O z -o $FILT/filtered_data_6.vcf.gz \
+        $FILT/filtered_data_5.vcf.gz
+
+bcftools stats $FILT/filtered_data_6.vcf.gz > $FILT/filtered_data_6.stats
+
+
+# strand bias
+bcftools filter -e 'SP<3' \
+        -O z -o $FILT/filtered_data_7.vcf.gz \
+        $FILT/filtered_data_6.vcf.gz
+
+bcftools stats $FILT/filtered_data_7.vcf.gz > $FILT/filtered_data_7.stats
+
+
+bcftools filter -Oz -S . -e 'FMT/GQ<20 | FMT/DP<5' \
+	-o $FILT/filtered_data_8.vcf.gz \
+	$FILT/filtered_data_7.vcf.gz
+
+bcftools stats $FILT/filtered_data_8.vcf.gz > $FILT/filtered_data_8.stats
+
+#bcftools filter -Oz -e 'F_MISSING>0.1' \
+#        -o $FILT/filtered_data_9.vcf.gz \
+#        $FILT/filtered_data_8.vcf.gz
+
+#bcftools stats $FILT/filtered_data_9.vcf.gz > $FILT/filtered_data_9.stats
+
